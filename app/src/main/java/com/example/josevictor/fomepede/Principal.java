9e3594c80b89f6a5fd5f2efa4061ddbdb7c67c04
Jose Victor <josevictorferreira@outlook.com>
2017-11-13 20:27:14 -0200

    Adicionado menu e criado activity de Cadastro de Restaurantes

--- app/src/main/java/com/example/josevictor/fomepede/Principal.java
+++ app/src/main/java/com/example/josevictor/fomepede/Principal.java
@@ -1,7 +1,14 @@
 package com.example.josevictor.fomepede;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class Principal extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.adicionar:
+                adicionaRestaurante();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void adicionaRestaurante() {
+        Intent intent = new Intent(this, CadastroRestaurante.class);
+        startActivity(intent);
     }
 }
