48c56b621e479d09897fa2246da7eb4d67bf375c
Jose Victor <josevictorferreira@outlook.com>
2017-11-13 20:48:34 -0200

    Inicio form de cadastros

--- app/src/main/java/com/example/josevictor/fomepede/Principal.java
+++ app/src/main/java/com/example/josevictor/fomepede/Principal.java
@@ -3,12 +3,9 @@ package com.example.josevictor.fomepede;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 
 public class Principal extends AppCompatActivity {
 
